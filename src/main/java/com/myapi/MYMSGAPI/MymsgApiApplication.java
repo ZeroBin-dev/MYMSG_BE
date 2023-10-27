5d8ba5499116b271a34862f877d9a315ecc866a1
CYB <zero-bin@naver.com>
2023-10-27 09:49:02 +0900

    first commit

---
+++ src/main/java/com/myapi/MYMSGAPI/MymsgApiApplication.java
@@ -0,0 +1,13 @@
+package com.myapi.MYMSGAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymsgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymsgApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapi/MYMSGAPI/MymsgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapi.MYMSGAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymsgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
