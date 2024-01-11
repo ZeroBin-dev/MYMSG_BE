bc19aaeab55b2ff546ecf4d9b644a2f634a70898
CYB <zero-bin@naver.com>
2024-01-11 09:56:34 +0900

    최신 소스 동기화

---
+++ src/main/java/com/myapi/mymsgapi/contoller/user/UserViewController.java
@@ -0,0 +1,28 @@
+package com.myapi.mymsgapi.contoller.user;
+
+import com.myapi.mymsgapi.comm.utils.SessionUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserViewController {
+
+  @RequestMapping(value = "/user/login", method = {RequestMethod.GET, RequestMethod.POST})
+  @Operation(summary = "로그인", description = "로그인 화면으로 이동")
+  public String userLogin(Model model) {
+    SessionUtil.removeUserVO();
+    return "views/user/login";
+  }
+
+  @GetMapping(value = "/user/chngPw")
+  @Operation(summary = "비밀번호 변경", description = "비밀번호 변경")
+  public String userChngPw(Model model) {
+    model.addAttribute("userId", "testUserId");
+    return "views/user/chngPw";
+  }
+
+}
